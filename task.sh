d20c01ccfe0646a1262442f3a4f31e9380840e09
Lisheng2016 <slireg2014@126.com>
2017-06-13 05:06:04 +0000

    adding task.sh

---
+++ task.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+
+dbname="task"
+table_layout="(id int not null auto_increment primary key,name varchar(255),description text)"
+action=$1
+list_name=$2
+name=$3
+id=$4
+desc=$5
+
+if [ $# -lt 1 ]
+then
+
+echo "*************************************"
+echo "Task application by jim li"
+echo "*************************************"
+echo ""
+echo "  Usage:"
+echo "  task (new-list|del-list|add|del|ls)"
+echo "  new-list: Create a new list"
+echo "  del-list: Delete a list and all tasks in the list"
+echo "  add: task add list_name task_name task_description - creates a enw task"
+echo "  del: task del list_name task_id - deletes a working task"
+echo "  ls: task ls - lists tasks,task ls list_name - lists tasks in a list"
+echo ""
+echo ""
+echo ""
+fi
+
+function create_list {
+	echo "CREATE TABLE ${list_name}${table_layout};" |mysql $dbname
+}
+
+function delete_list {
+	echo "DROP TABLE ${list_name};" |mysql $dbname
+}
+
+function add_task {
+	echo "INSERT INTO ${list_name} (name,description) VALUES ($name,$desc);" | mysql $dbname
+}
+
+function del_task {
+	echo "DELETE FROM ${list_name} WHERE name='$name';" | mysql $dbname
+}
+
+function list_task {
+	if [ -z $2 ]
+	then
+		echo "SHOW TABLES;" | mysql $dbname
+	else 
+		echo "SELECT * FROM $list_name;" | mysql $dbname
+}
